96a0089e0468a4086fce88405581a00d2de6dde7
Henrique Guerra Messias <hgmessias@hotmail.com>
2024-03-27 11:52:04 -0300

    realizando modificacoes no desafio 3

--- desafio2.js
+++ desafio2.js
@@ -1,35 +1,33 @@
 
 function CalculoIMC(Peso, Altura) {
    const
-    Peso = window.prompt('Digite seu peso:'),
-    Altura = window.prompt('Digite sua altura:'),
-    imc = peso / altura ** 2;
- 
-    const imc = Peso / Altura ** 2;
+    imc = Peso/ Altura ** 2;
     let mensagem;
-    
-
+  
 if (imc < 18.5) {
-   mensagem`IMC: ${imc.toFixed(1)} --> Abaixo do peso`;
+   mensagem = `IMC: ${imc.toFixed(1)} --> Abaixo do peso`;
 
 } else if (imc < 25) {
-    mensagem`IMC: ${imc.toFixed(1)} --> Peso normal`;
+    mensagem = `IMC: ${imc.toFixed(1)} --> Peso normal`;
 
 } else if (imc < 30) {
-    mensagem`IMC: ${imc.toFixed(1)} --> Sobrepeso`;
+    mensagem = `IMC: ${imc.toFixed(1)} --> Sobrepeso`;
 
 } else if (imc < 35) {
-    mensagem`IMC: ${imc.toFixed(1)} --> Obesidade grau I`;
+    mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau I`;
 
 } else if (imc < 40) {
-    mensagem`IMC: ${imc.toFixed(1)} --> Obesidade grau II`;
+    mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau II`;
 
 } else {
-   mensagem`IMC: ${imc.toFixed(1)} --> Obesidade grau III`;
-}
+   mensagem = `IMC: ${imc.toFixed(1)} --> Obesidade grau III`;
 }
 
 return mensagem;
+
+}
+
+
 
 //Crie um programa que calcule o Índice de Massa Corporal (IMC) de uma pessoa.
 
