1442d06a43c61161405fbc3a07526e4e76077043
JuliaVitoriaS <juliavitoriaferreirasantos2018@gmail.com>
2024-05-06 11:08:58 -0300

    feat: conseguindo resolver as divergencias no código

--- js/navbar.js
+++ js/navbar.js
@@ -1,37 +1,38 @@
 //Ancora do exercício 1
 const ancoras = [
-{ "url": "/index.html", "texto": "Home"},
-{ "url": "exercicios/exercicios1.html" ,"texto": "Exerc.1"},
-{ "url": "exercicios/exercicios3.html" ,"texto": "Exerc.3"},
+    { "url": "/index.html", "texto": "Home"},
+    { "url": "exercicios/exercicios1.html" ,"texto": "Exerc.1"},
+    { "url": "exercicios/exercicios3.html" ,"texto": "Exerc.3"},
+    { "url": "PlacarDeJogo.html", "texto": "Exerc.PlacarJogo"},
 ];
 
 
 function criaAncora (url, texto){
     const ancora = document.createElement('a');
-    ancora.setAttribute('href', '/index.html');
+    ancora.setAttribute('href', url);
     ancora.innerHTML = texto;
     
      return ancora;
 }
 
-function criarAngoraLi (ancora ){
+function criarAncoraLi (ancora ){
     const li = document.createElement ('li');
     li.appendChild(ancora);
-  
 
     return li;
 
+}
 
 function criaUlNav(ancoras) {
   const ul = document.createElement ('ul');
-    ancoras.foreEach(ancora => {
+    ancoras.forEach(ancora => {
         const a = criaAncora(ancora.url, ancora.texto);
-        const li = criarAngoraLi(a);
+        const li = criarAncoraLi(a);
         ul.appendChild(li);
 
 
     })
-    
+  return ul;
 }
 
 
@@ -57,48 +58,46 @@ function criaUlNav(ancoras) {
 
 
 
-//<ul> do menu
-const ulNav = document.createElement ('ul');
-ulNav.append(liHome, liExercicios)
 
 
 
 //<nav> do menu
 const nav = document.createElement ('nav');
-nav.classList.add ('menu-navegacao');
-nav.appendChild (ulNav);
+nav.classList.add('menu-navegacao');
+nav.appendChild(criaUlNav(ancoras));
+console.log(criaUlNav(ancoras));
  
 // aqui segue  de acordo com a solução do professor 
 //<span> Desafios & Exercicios <span>
 
-const spanLogo =document.createElement('div');
-divMenuLogo.classList.add('logo');
+const spanlogo =document.createElement('span');
+spanlogo.classList.add('logo');
 
 //<div class="logo-menu">
 
 const divlogo =document.createElement('div');
-divlogo.classList.add('logo-menu');
-divlogo.append(divlogo, nav);
-
-
+divlogo.classList.add('logo');
+divlogo.append(spanlogo);
 
 const divMenuLogo =document.createElement('div');
 divMenuLogo.classList.add('logo-menu');
-divMenuLogo.appendChild(nav);
+divMenuLogo.append(divlogo, nav);
+
+
 
 const header =document.createElement ('header');
 header.appendChild(divMenuLogo);
 
-document.body.append(nav); 
+
 
 // document.body.insertBefore(nav, document.body.firstChild);
 document.body.insertAdjacentElement('afterbegin', header);
 
 const linkNavbar = document.createElement('link');
-linkNavbar.setAttribute('rel', 'steyleheet');
-linkNavbar.setAttribute('href', '/css/navbar.css');
+linkNavbar.setAttribute('rel', 'stylesheet');
+linkNavbar.setAttribute('href', '/navbar.css');
 
-document.head.appendChild(linkNavbar);}
+document.head.appendChild(linkNavbar);
 
 
 // function criaAncora (url, texto){
